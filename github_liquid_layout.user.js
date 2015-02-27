0fb5dee588206cab6481426483f648e2a0fb5454
Hosoyama Terunobu <hosoyama@mediba.jp>
2015-02-27 14:32:03 +0900

    プルリク更新フォームがズレるのを修正

--- github_liquid_layout.user.js
+++ github_liquid_layout.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         GitHubリキッドレイアウト
 // @namespace    https://github.com/hosoyama-mediba/userscript/
-// @version      0.2
+// @version      0.3
 // @description  GitHubを横幅固定からリキッドレイアウトにします
 // @author       Terunobu Hosoyama <hosoyama@mediba.jp>
 // @match        https://github.com/*/*
@@ -11,56 +11,48 @@
 (function() {
     var style = document.createElement('style');
     style.textContent = (function () {/*
+        html {
+            overflow-y: scroll !important;
+        }
         .blob-code {
             font-family: Ricty,Consolas, "Liberation Mono", Menlo, Courier, monospace !important;
             font-size: 14px !important;
          }
-
-        html {
-            overflow-y: scroll !important;
-        }
-
-
         .wrapper {
             padding: 0 15px !important;
         }
-
         .container {
             width: 100% !important;
         }
-
         #js-repo-pjax-container {
             width: 100% !important;
             padding-right: 38px !important;
         }
-
         .only-with-full-nav,
         .full-word {
             display: none !important;
         }
-
         .repository-sidebar {
             position: absolute !important;
             right: 0 !important;
             width: 38px !important;
         }
-
         .repository-sidebar .counter {
             display: none !important;
         }
-
         #discussion_bucket {
             margin-right: 160px !important;
+            position: relative !important;
         }
         .discussion-sidebar {
-            position:relative !important;
-            left: 160px !important;
+            position: absolute !important;
+            right: 0 !important;
+            margin-right: -160px;
         }
         .discussion-timeline {
             width: 100% !important;
             float: none !important;
         }
-
         .timeline-new-comment,
         .inline-comments,
         .inline-comment-form,
