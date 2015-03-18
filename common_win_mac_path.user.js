e07d14c01558d2a480b16b46809d607f356c0fac
Hosoyama Terunobu <hosoyama@mediba.jp>
2015-03-18 17:50:50 +0900

    表示されるURLをリンクに変更

--- common_win_mac_path.user.js
+++ common_win_mac_path.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Win⇔Macファイルパス変換
 // @namespace    https://github.com/hosoyama-mediba/userscript
-// @version      0.5
+// @version      0.6
 // @description  TalknoteかRedmine上でファイルサーバのパスを選択するとWin,Mac用に変換したパスを表示します
 // @author       Terunobu Hosoyama <hosoyama@mediba.jp>
 // @match        https://company.talknote.com/mediba.jp/*
@@ -17,7 +17,7 @@
             display: block;
             margin: 0;
             padding: 0;
-            border: 1px solid #66f;
+            border: 1px solid #169;
             border-radius: 5px;
             background-color: #fff;
             color: #336;
@@ -31,18 +31,31 @@
             margin-left: -300px;
             line-height: 1;
         }
-        .win-mac-path-area p {
-            margin: 10px;
+        .win-mac-path-area a {
+            display: block;
+            margin: 0;
+            padding: 10px;
+            color: #169;
+            text-decoration: none;
+        }
+        .win-mac-path-area a:hover {
+            color: #c44;
+        }
+        .win-mac-path-area a:focus {
+            outline: 3px solid #49c;
+            outline-radius: 5px;
+            outline-offset: -1px;
         }
         .win-mac-path-area hr {
             display: block;
-            border: 1px solid #66f;
+            margin: 0;
+            border: 1px solid #169;
             border-width:1px 0 0 0;
         }
     */}).toString().replace(/(\n)/g, '').split('*')[1];
     document.getElementsByTagName('head')[0].appendChild(style);
     $(document).on('mouseup', function(e) {
-        var sel, range, contents, target, matches, win2mac = true, $target = $(e.target);
+        var sel, range, contents, target, matches, win2mac = true, $target = $(e.target), url;
 
         if ($target.hasClass('win-mac-path-area') || $target.parent().hasClass('win-mac-path-area')) {
             return;
@@ -73,17 +86,17 @@
         }
 
         $('body').append($('<div class="win-mac-path-area"></div>'));
-        $('.win-mac-path-area').append($('<p class="">' + target + '</p>'));
+        $('.win-mac-path-area').append($('<a href="' + target + '" tabindex="10001">' + target + '</a>'));
         $('.win-mac-path-area').append($('<hr>'));
 
-        if (win2mac) {
-            $('.win-mac-path-area').append($('<p class="">' + 'smb://file03/fileshare' + matches[1].replace(/\\/g, '/') + '</p>'));
-        } else {
-            $('.win-mac-path-area').append($('<p class="">' + 'W:' + matches[1].replace(/\//g, '\\') + '</p>'));
-        }
+        url = win2mac
+            ? 'smb://file03/fileshare' + matches[1].replace(/\\/g, '/')
+            : 'W:' + matches[1].replace(/\//g, '\\');
+        $('.win-mac-path-area').append($('<a href="' + url + '" tabindex="10002">' +  url + '</a>'));
 
         range = document.createRange();
-        range.selectNodeContents($('.win-mac-path-area').get(0).firstChild);
+        range.selectNodeContents($('.win-mac-path-area a').get(0).firstChild);
+        $('.win-mac-path-area a').get(0).focus();
 
         sel.removeAllRanges();
         sel.addRange(range);
