6bc3119c543d83052d3e860ac58da50591fdefd4
Hosoyama Terunobu <hosoyama@mediba.jp>
2015-03-25 16:29:14 +0900

    Create redmine_news.user.js

---
+++ redmine_news.user.js
@@ -0,0 +1,79 @@
+// ==UserScript==
+// @name        Redmineのニュースでチケットのプレビュー表示
+// @namespace   https://github.com/hosoyama-mediba/userscript
+// @version     0.1
+// @description Redmineのニュースでチケットのプレビュー表示します。
+// @author      Terunobu Hosoyama <hosoyama@mediba.jp>
+// @match       http://au-project.mediba.local/news/*
+// ==/UserScript==
+
+(function() {
+    $('head').append($('<style/>').text((function () {/*
+        .ex-issue_container {
+            display:none;
+            position: fixed;
+            left: 6em;
+            top: 40px;
+            max-width: 800px;
+            max-height: 85%;
+            border: 1px solid #666;
+            border-radius: 4px;
+            margin: 0;
+            padding: 10px;
+            box-shadow: 10px 10px 10px rgba(0,0,0,0.4);
+            background-color: #fff;
+            overflow: auto;
+        }
+        .ex-issue_container #ex-content {
+            width: 100%;
+            border: none;
+            background-color: #fff;
+        }
+        .noscroll {
+            overflow: hidden;
+            margin-right: 15px;
+        }
+    */}).toString().replace(/(\n)/g, '').split('*')[1]));
+    
+    $container = $('<div class="ex-issue_container"/>');
+    $('body').append($container);
+
+    $('#wrapper').on('click', function(e) {
+        $container.is(':visible') && $container.hide('fast');
+        $('html').removeClass('noscroll');
+    });
+
+    
+    $(window).on('mousewheel', function(e) {
+        console.log(e.target);
+    });
+    
+    $('a.issue, a.external[href^="http://au-project.mediba.local/issues/"]').each(function() {
+        $issue = $(this);
+        $issue.on('mouseover', function(e) {
+            if ($container.is(':visible')) {
+                return;
+            }
+            $container.html('<img src="http://au-project.mediba.local/images/loading.gif">');
+            $container.show('fast');
+            $.ajax({
+                url: this.href,
+                timeout: 3000,
+                success: function(data) {
+                    var $innerIssue = $(data).find('#content').attr('id', 'ex-content');
+                    $innerIssue.find('script').text('');
+                    $container.html($innerIssue);
+                    $('html').addClass('noscroll');
+                },
+                error: function(data) {
+                    $container.html('<img src="http://au-project.mediba.local/images/delete.png">');
+                    setTimeout(function() {
+                        $container.hide('fast');
+                        $('html').removeClass('noscroll');
+                    }, 1000);
+                }
+            });
+            return false;
+        });
+    });
+})();
