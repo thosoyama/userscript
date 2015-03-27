a7383fb3e30ef8e80f4d10cc7bc0943a6269d798
Hosoyama Terunobu <hosoyama@mediba.jp>
2015-03-27 10:03:22 +0900

    もろもろ修正
    * ハッシュタグに限らず検索ワードのブックマーク機能にした
    * jQuery使うようにした
    * jsonpをdevelopに置くのを止めてJenkinsのuserContentに置くようにした

--- thashtag.user.js
+++ thashtag.user.js
@@ -1,52 +1,125 @@
 // ==UserScript==
 // @name        THashTag
 // @namespace   https://github.com/hosoyama-mediba/userscript
-// @version     0.2
-// @description Talknoteの左メニューにハッシュタグのリンクを追加します
+// @version     0.3
+// @description Talknoteの左メニューに検索ワードのリンクを追加します
 // @include     https://company.talknote.com/mediba.jp/*
 // @exclude
-// @require     http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js
+// @require     https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js
 // @grant       none
 // ==/UserScript==
 
 (function() {
-    var scriptText = (function () {/*
+    var $style = $('<style/>').text((function () {/*
+        .thashtag-form__span {
+            line-height: 1;
+            height: 27px;
+            display: block;
+            background: url(/images/navi_bg170x27.png) 0 0 no-repeat;
+            background-position: 0 0;
+            padding: 0 8px;
+            font-weight: bold;
+            font-size: 12px;
+            text-decoration: none;
+            overflow: hidden;
+            text-overflow: ellipsis;
+            white-space: nowrap;
+            position: relative;
+        }
+        .thashtag-form__text {
+            width: 100px;
+        }
+        .thashtag-form__add {
+            position: absolute;
+            right: 0;
+            margin-right: 8px;
+            line-height: 20px;
+        }
+        .thashtag-form__add:hover {
+            text-decoration: underline;
+        }
+        .thashtag-form__del {
+            position: absolute;
+            right: 0;
+            margin-right: 8px;
+        }
+        .thashtag-form__del:hover {
+            font-weight: bold;
+            color: #f66;
+            text-decoration: underline;
+        }
+
+    */}).toString().split('*')[1]);
+
+    var $script = $('<script/>').text((function () {/*
         window.tHashTagCallBack = function(response) {
-            var tags = response || [
-                'あさかい報告',
-                'スプリントレビュー',
-                'アルファ',
-                'ベータ',
-                'お悩み',
+            var words, $fragment, createNewLink, setting, i, l, urlPrefix = '/mediba.jp/search?keyword=';
+
+            $.cookie.json = true;
+
+            words = response || [
+                '#あさかい報告',
+                '#スプリントレビュー',
+                '#アルファ',
+                '#ベータ',
+                '#お悩み'
             ];
-            var leftMenu = document.getElementsByClassName('left_menu')[0];
-            if (!leftMenu) {
-                return;
+
+            createNewLink = function(word, necessary) {
+                var $link = $('<a/>').attr({href: urlPrefix + encodeURIComponent(word), title: word}).html(word);
+                if (!necessary) {
+                    var $deleteButton = $('<span class="thashtag-form__del" data-key="' + word + '" title="削除">✕</span>');
+                    $deleteButton.on('click', function() {
+                        var setting = $.cookie('thashtag') || {};
+                        delete setting[$(this).data('key')];
+                        $.cookie('thashtag', setting, {path: '/', expires: 365});
+                        $(this).parent().remove();
+                        return false;
+                    });
+                    $link.append($deleteButton);
+                }
+                return $link;
+            };
+
+            $fragment = $(document.createDocumentFragment());
+            for (i = 0, l = words.length; i < l; i++) {
+                $fragment.append(createNewLink(words[i], true));
             }
-            var tagRoot = document.createElement('li');
-            leftMenu.appendChild(tagRoot);
-            var titleLink = document.createElement('a');
-            titleLink.href = '#';
-            titleLink.innerHTML = 'ハッシュタグ';
-            tagRoot.appendChild(titleLink);
-            var tagUlist = document.createElement('ul');
-            tagRoot.appendChild(tagUlist);
-            var tagList = document.createElement('li');
-            tagUlist.appendChild(tagList);
-            for (var i = 0, l = tags.length; i < l; i++) {
-                var tagLink = document.createElement('a');
-                tagLink.href = '/mediba.jp/search?keyword=%23' + encodeURIComponent(tags[i]);
-                tagLink.innerHTML = '#' + tags[i];
-                tagList.appendChild(tagLink);
+            setting = $.cookie('thashtag') || {};
+            for (i in setting) {
+                $fragment.append(createNewLink(i));
             }
+
+            $('.left_menu').append($('<li><a>検索ブックマーク</a><ul><li class="thashtag-links"></li></ul></li>'));
+            $('.thashtag-links').append($fragment);
+
+            $('.left_menu > li:last-child').append('<ul><li class="thashtag-form"><span class="thashtag-form__span"><input type="text" class="thashtag-form__text"><button class="thashtag-form__add">追加</button></span></li></ul>');
+            $('.thashtag-form__text').keypress(function(e) {
+                if (e.which === 13) {
+                    return $('.thashtag-form__add').click();
+                }
+            });
+            $('.thashtag-form__add').on('click', function() {
+                var value = $('.thashtag-form__text').val().trim();
+                if (!value.length) {
+                    return false;
+                }
+                var setting = $.cookie('thashtag') || {};
+                if (value in setting || $.inArray(value, words) >= 0) {
+                    alert('「' + value + '」は既に登録済です');
+                    return false;
+                }
+                setting[value] = 1;
+                $.cookie('thashtag', setting, {path: '/', expires: 365});
+                $('.thashtag-links').append(createNewLink(value));
+                $('.thashtag-form__text').val('');
+                return false;
+            });
         };
-    */}).toString().replace(/(\n)/g, '').split('*')[1];
+    */}).toString().replace(/(\n)/g, '').split('*')[1]);
 
-    var script = document.createElement('script');
-    script.textContent = scriptText;
-    document.body.appendChild(script);
-
-    script = document.createElement('script');
-    script.src = 'https://develop.mdev.auone.jp/webapi/thashtag.js';
-    document.body.appendChild(script);
+    $('head')
+        .append($style)
+        .append($script)
+        .append($('<script src="https://alpha.mdev.auone.jp/jenkins/userContent/thashtag.jsonp"/>'));
 })();
