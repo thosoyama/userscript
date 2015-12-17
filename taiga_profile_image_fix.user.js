e7df7d1e33e571ed6921b7b876dc1c75bbaa6633
Hosoyama Terunobu <hosoyama@mediba.jp>
2015-12-17 13:54:35 +0900

    ( ˙-˙ )
    ( ˙-˙ )

---
+++ taiga_profile_image_fix.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         Taigaのプロフィール画像を表示する( ˙-˙ )
+// @namespace    https://github.com/hosoyama-mediba/userscript
+// @version      0.1
+// @name         なおるまで( ˙-˙ )
+// @author       Terunobu Hosoyama <hosoyama@mediba.jp>
+// @match        http://taiga.mediba.jp/project/mediba-dolphin/
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js
+// @grant        none
+// ==/UserScript==
+
+(function($) {
+    'use strict';
+    var observer = new MutationObserver(function (mutations) {
+        mutations.forEach(function (mutation) {
+            var $brokenImg = $(mutation.target).find('img[src*=localhost\\:8000]');
+            console.dir($brokenImg[0]);
+            $brokenImg.each(function() {
+                this.src = this.src.replace(/localhost\:8000/, 'taiga.mediba.jp');
+            });
+        })
+    });
+    observer.observe(document.body, { childList: true, subtree: true });
+})(jQuery.noConflict(true));
